408d784d1f1ed62057779bd1e8138156442a0b1d
jstnswn <jstnswn@gmail.com>
2022-02-03 11:04:47 -0800

    Basic React and Redux setup complete with proxy and csrf handling

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,5 +10,36 @@ router.get('/hello/world', function (req, res) {
   res.send('Hello World!');
 });
 
+// Static routes
+
+// In Production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve('../frontend/build')));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+// In Development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
 
 module.exports = router;
