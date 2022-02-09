ff7e84236a0a3e0079bfd79c46e82a8c7df2fa69
jstnswn <jstnswn@gmail.com>
2022-02-08 23:36:38 -0800

    Create Album component for each mapped album for easier isolated event handling

--- frontend/src/components/Dashboard/Albums/AlbumsGrid.js
+++ frontend/src/components/Dashboard/Albums/AlbumsGrid.js
@@ -1,55 +1,22 @@
 import React, { useState } from 'react';
+import Album from './Album';
 import './AlbumGrid.css';
 
 export default function AlbumsGrid({ albums }) {
   const [showIdx, setShowIdx] = useState(null);
-  const [showDiv, setShowDiv] = useState(false);
+
 
   const albums2 = albums.reverse();
 
   const openModal = (idx) => setShowIdx(idx);
   const closeModal = () => setShowIdx(null);
 
-  const showOptions = (e) => {
-    if (showDiv) return;
-    setShowDiv(true);
-  };
-
-  const hideOptions = (e) => {
-    if (!showDiv) return;
-    setShowDiv(false);
-  };
 
 
   return (
     <div className='album-grid'>
       {albums2.map((album, idx) => (
-        <div className='album-container' key={idx}>
-          <img
-            className='album-grid-image'
-            alt={album.title}
-            src={album.images[0].imageUrl}
-            style={{
-              gridColumnStart: idx % 3 + 1
-            }}
-            onMouseEnter={showOptions}
-            onMouseLeave={hideOptions}
-          />
-          <div
-            className='album-info'
-            style={{bottom: showDiv ? '10px' : '-23px'}}
-          >
-            <h4>{album.title}</h4>
-            <p>{`${album.images.length} images`}</p>
-            <div
-              className='album-actions'
-
-            >
-              <i className='fal fa-trash'></i>
-              <i className='far fa-edit'></i>
-            </div>
-          </div>
-        </div>
+        <Album key={idx} album={album} idx={idx}/>
       ))}
     </div>
   );
