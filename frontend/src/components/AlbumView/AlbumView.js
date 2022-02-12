f3df71b96b04a054cd90683332c23f5d2f4ac172
jstnswn <jstnswn@gmail.com>
2022-02-12 12:26:43 -0800

    Setup basic album view popup with option to enlarge individual images

---
+++ frontend/src/components/AlbumView/AlbumView.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+import './AlbumView.css';
+
+export default function AlbumView({ album, closeAlumView }) {
+  const [selectedImage, setSelectedImage] = useState(album?.images[0])
+  console.log('album: ', album)
+
+  if (!selectedImage) return <h2>You don't have any alums</h2>
+
+  return (
+    <div className='album-view-container'>
+      <div className='album-view album-container'>
+        <img
+          className='album-view album-image'
+          alt={selectedImage.title}
+          src={selectedImage.imageUrl}
+        />
+      </div>
+      <div className='album-view footer'>
+        <div className='album-view image-selector'>
+          {album.images.map((image, idx) => (
+            <div className='album-view image-container'>
+              <img
+                className='album-view image'
+                onClick={() => setSelectedImage(image)}
+                alt={image.title}
+                src={image.imageUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+    </div>
+  )
+}
